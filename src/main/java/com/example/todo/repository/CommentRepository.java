c2873e5a90bc88ac0861efa33e1dd5324ec73c7b
yudonggeun <ydong98@gmail.com>
2023-11-13 12:10:41 +0900

    feat : 할일 삭제 기능 구현

--- src/main/java/com/example/todo/repository/CommentRepository.java
+++ src/main/java/com/example/todo/repository/CommentRepository.java
@@ -4,4 +4,5 @@ import com.example.todo.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    void deleteByTodoId(Long id);
 }
