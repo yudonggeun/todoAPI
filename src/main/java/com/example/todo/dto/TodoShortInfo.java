5de212f0ee221899766b334eabb9b98bd17659c4
yudonggeun <ydong98@gmail.com>
2023-11-17 18:49:38 +0900

    test : 할일 조회 API 테스트 구현
    1. 할일 상세 조회 테스트
    2. 할일 목록 조회 테스트

--- src/main/java/com/example/todo/dto/TodoShortInfo.java
+++ src/main/java/com/example/todo/dto/TodoShortInfo.java
@@ -1,6 +1,7 @@
 package com.example.todo.dto;
 
 import com.example.todo.domain.Todo;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ public record TodoShortInfo(
         @Schema(description = "할일 제목", example = "스프링 정복하기")
         String title,
         @Schema(description = "할일 상세 내용", example = "spring mvc, security, jpa 정복!")
+        @JsonInclude(JsonInclude.Include.NON_NULL)
         String content,
         @Schema(description = "할일 작성시간", example = "2023-11-11T20:43:18.170")
         LocalDateTime createdAt
--- src/test/java/com/example/todo/controller/TodoControllerTest.java
+++ src/test/java/com/example/todo/controller/TodoControllerTest.java
@@ -1,25 +1,29 @@
 package com.example.todo.controller;
 
+import com.example.todo.domain.Todo;
 import com.example.todo.dto.request.CreateTodoRequest;
 import com.example.todo.dto.request.UpdateTodoRequest;
+import com.example.todo.repository.CommentRepository;
+import com.example.todo.repository.TodoRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class TodoControllerTest extends ControllerTest {
-
 
     @DisplayName("인증하지 않은 경우")
     @Nested
     class NoAuthenticatedCases {
+
         @DisplayName("할일 생성 실패")
         @Test
         void createTodoFailWhenNoUser() throws Exception {
@@ -93,5 +97,120 @@ class TodoControllerTest extends ControllerTest {
                     jsonPath("$.message").value("토큰이 유효하지 않습니다.")};
         }
 
+    }
+
+    @Autowired
+    TodoRepository todoRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+    @AfterEach
+    void clear() {
+        commentRepository.deleteAll();
+        todoRepository.deleteAll();
+    }
+
+    @DisplayName("할일 생성 성공")
+    @Test
+    void createTodo() throws Exception {
+        // given
+        var title = "스프링 정복하기";
+        var content = "안할거지롱";
+        var request = new CreateTodoRequest(title, content);
+        // when // then
+        mockMvc.perform(post("/todo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request))
+                .with(user("testuser"))
+        ).andDo(print()
+        ).andExpectAll(
+                status().isOk(),
+                jsonPath("$.id").exists(),
+                jsonPath("$.author").value("testuser"),
+                jsonPath("$.title").value(title),
+                jsonPath("$.content").value(content),
+                jsonPath("$.createdAt").exists(),
+                jsonPath("$.content").exists()
+        );
+    }
+
+    @DisplayName("할일 상세 조회 성공")
+    @Test
+    void getTodo() throws Exception {
+        // given
+        Todo todo = saveTodo("testuser", "test", "test content");
+        // when // then
+        mockMvc.perform(get("/todo/" + todo.getId())
+                        .with(user("testuser")))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.id").exists(),
+                        jsonPath("$.author").value("testuser"),
+                        jsonPath("$.title").value(todo.getTitle()),
+                        jsonPath("$.content").value(todo.getContent()),
+                        jsonPath("$.createdAt").exists(),
+                        jsonPath("$.content").exists()
+                );
+    }
+
+    @DisplayName("할일 상세 조회 실패")
+    @Test
+    void getTodoWhenIsNotAuthor() throws Exception {
+        // given
+        Todo todo = saveTodo("hacker", "test", "test content");
+        // when // then
+        mockMvc.perform(get("/todo/" + todo.getId())
+                        .with(user("testuser")))
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        jsonPath("$.status").value("unauthorized"),
+                        jsonPath("$.message").value("작성자만 조회할 수 있습니다.")
+                );
+    }
+
+    @DisplayName("할일 목록 조회 성공")
+    @Test
+    void getList() throws Exception {
+        // given
+        var titleCondition = "스프링";
+        saveTodo("testuser", "스프링 시큐리티", "test1");
+        saveTodo("testuser", "다시보는 스프링", "test2");
+        saveTodo("otheruser", "다시보는 스프링", "test2");
+        saveTodo("testuser", "다시보는 자바", "test3");
+        // when // then
+        mockMvc.perform(get("/todo")
+                        .param("title", titleCondition)
+                        .with(user("testuser"))
+                )
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        jsonPath("$.size").value(2),
+
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[0].id").exists(),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[0].title").value("다시보는 스프링"),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[0].content").value("test2"),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[0].createdAt").exists(),
+
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[1].id").exists(),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[1].title").value("스프링 시큐리티"),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[1].content").value("test1"),
+                        jsonPath("$.data[?(@.author == \"testuser\")].todoList[1].createdAt").exists(),
+
+                        jsonPath("$.data[?(@.author == \"otheruser\")].todoList[0].id").exists(),
+                        jsonPath("$.data[?(@.author == \"otheruser\")].todoList[0].title").value("다시보는 스프링"),
+                        jsonPath("$.data[?(@.author == \"otheruser\")].todoList[0].content").isEmpty(),
+                        jsonPath("$.data[?(@.author == \"otheruser\")].todoList[0].createdAt").exists()
+                );
+    }
+
+    private Todo saveTodo(String author, String title, String content) {
+        return todoRepository.saveAndFlush(Todo.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build());
     }
 }
