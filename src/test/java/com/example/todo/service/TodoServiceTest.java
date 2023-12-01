9f85ddf75f3fe6c11ec84ec44fb31a09caecd893
yudonggeun <ydong98@gmail.com>
2023-12-01 10:44:35 +0900

    test(todo): 할일 수정 테스트
    # 구현 테스트 케이스
    - 정상 동작 테스트
    - 작성자가 아닌 사람이 수정하는 예외 케이스 테스트
    - 존재하지 않은 할일 수정 예외 케이스 테스트

--- src/test/java/com/example/todo/service/TodoServiceTest.java
+++ src/test/java/com/example/todo/service/TodoServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.todo.service;
 
+import com.example.todo.common.exception.NotExistException;
 import com.example.todo.domain.Todo;
 import com.example.todo.dto.TodoInfo;
+import com.example.todo.dto.request.UpdateTodoRequest;
 import com.example.todo.repository.CommentRepository;
 import com.example.todo.repository.TodoRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +19,7 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -76,7 +79,60 @@ class TodoServiceTest {
         assertThat(info.title()).isEqualTo(title);
     }
 
-    private String randomString(){
+    private String randomString() {
         return UUID.randomUUID().toString();
     }
+
+    @DisplayName("작성자가 할일을 업데이트하고 반영된 내용을 반환할 수 있다.")
+    @Test
+    void update_when_author_request_then_return_updated_todo_info() {
+        // given
+        var todo = Todo.builder()
+                .author("유동근")
+                .content("잘하자")
+                .title("TIL?")
+                .build();
+
+        var request = new UpdateTodoRequest(1L, "TIL 쓰기", "그냥 하지 말까?");
+        given(repository.findById(any())).willReturn(Optional.of(todo));
+        given(loginStatusService.getLoginCustomerName()).willReturn("유동근");
+        // when
+        TodoInfo info = service.updateTodo(request);
+        // then
+        assertThat(info.title()).isEqualTo("TIL 쓰기");
+        assertThat(info.content()).isEqualTo("그냥 하지 말까?");
+    }
+
+    @DisplayName("작성자가 아니라면 할일을 수정할 수 없다.")
+    @Test
+    void update_when_other_request_then_throw_AccessDeniedException() {
+        // given
+        var todo = Todo.builder()
+                .author("유동근")
+                .content("잘하자")
+                .title("TIL?")
+                .build();
+
+        var request = new UpdateTodoRequest(1L, "TIL 쓰기", "그냥 하지 말까?");
+        given(repository.findById(any())).willReturn(Optional.of(todo));
+        given(loginStatusService.getLoginCustomerName()).willReturn("jon");
+        // when // then
+        assertThatThrownBy(() -> service.updateTodo(request))
+                .isInstanceOf(AccessDeniedException.class)
+                .hasMessage("작성자만 삭제/수정할 수 있습니다.");
+    }
+
+    @DisplayName("존재하지 않은 할일 수정할 수 없다.")
+    @Test
+    void update_when_access_not_exist_todo_then_throw_NotExistException() {
+        // given
+        var request = new UpdateTodoRequest(1L, "TIL 쓰기", "그냥 하지 말까?");
+        given(repository.findById(any())).willReturn(Optional.empty());
+        given(loginStatusService.getLoginCustomerName()).willReturn("jon");
+        // when // then
+        assertThatThrownBy(() -> service.updateTodo(request))
+                .isInstanceOf(NotExistException.class)
+                .hasMessage("존재하지 않은 할일 목록입니다.");
+    }
+
 }
